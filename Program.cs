48a14b34218220596ae76f6845a9a9f6fa15f68c
konstantin bondarenko <zois1990@gmail.com>
2022-08-18 17:24:47 +0200

    task 25 complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+/*
+int Exp (int num1, int num2)
+{
+    int result = 1;
+    for(;num2 > 0; num2--)
+        result = result * num1;
+    return result;
+}
+int ReadNumbers (string str)
+{
+    Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = ReadNumbers("Input number: ");
+int degree =  ReadNumbers("Input degree: ");
+Console.WriteLine(Exp(number, degree));
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
